a99d5c36aace4ceabd3c39b28ae3c4a7fe1fae6c
se-escamilla98 <se.escamilla@duocuc.cl>
2025-07-01 19:34:42 -0400

    DuenosController.java
    codigo corregido

--- src/main/java/AnimalCare/InformacionAtenciones/controller/DuenosController.java
+++ src/main/java/AnimalCare/InformacionAtenciones/controller/DuenosController.java
@@ -1,7 +1,7 @@
 package AnimalCare.InformacionAtenciones.controller;
 
 import AnimalCare.InformacionAtenciones.model.Duenos;
-import AnimalCare.InformacionAtenciones.services.DuenosInterface;
+import AnimalCare.InformacionAtenciones.services.DuenosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,52 +11,45 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/duenos_mascotas")
-
-
 public class DuenosController {
 
     @Autowired
-    private DuenosInterface duenosInterface;
-
+    private DuenosService duenosService;
 
     @GetMapping
     public List<Duenos> listarDuenos() {
-        return duenosInterface.listarDuenos();
+        return duenosService.listarDueno();
     }
-
 
     @PostMapping
     public Duenos registrarDuenos(@RequestBody Duenos duenos) {
-        return DuenosInterface.registrarDuenos(duenos);
+        return duenosService.registrarDuenos(duenos);
     }
-
 
     @GetMapping("/{id}")
-    public ResponseEntity<Duenos> obtenerDuenosPorId(@PathVariable Long id) {
-        Optional<Duenos> duenos = duenosInterface.obtenerDuenoPorId(id);
+    public ResponseEntity<Duenos> obtenerDuenosPorId(@PathVariable Integer id) {
+        Optional<Duenos> duenos = duenosService.obtenerDuenosPorId(id);
         return duenos.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
-
 
     @PutMapping("/{id}")
-    public ResponseEntity<Duenos> actualizarDuenos(@PathVariable Long id, @RequestBody Duenos duenosActualizado) {
-        Optional<Duenos> duenos = duenosInterface.actualizarDuenos(id, duenosActualizado);
+    public ResponseEntity<Duenos> actualizarDuenos(@PathVariable Integer id, @RequestBody Duenos duenosActualizado) {
+        Optional<Duenos> duenos = duenosService.actualizarDuenos(id, duenosActualizado);
         return duenos.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminarCliente(@PathVariable Long id) {
-        boolean eliminado = duenosInterface.eliminarDuenos(id);
+    public ResponseEntity<Void> eliminarCliente(@PathVariable Integer id) {
+        boolean eliminado = duenosService.eliminarDuenos(id);
         return eliminado ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 
-
     @GetMapping("/buscar")
-    public List<Duenos> buscarPorNombre(@RequestParam String nombre) {
-        return duenosInterface.buscarPorNombre(nombre);
+    public ResponseEntity<Duenos> buscarPorCorreo(@RequestParam String correoElectronico) {
+        Optional<Duenos> dueno = duenosService.buscarPorCorreoElectronico(correoElectronico);
+        return dueno.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
-
 }
